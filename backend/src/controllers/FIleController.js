4d1e972b278f45f1d135390dddb1efdf2abb28fc
clediano <cledianoestefenon@gmail.com>
2019-04-14 00:39:57 -0300

    Criado frontend da aplicação usando ReactJS.

--- backend/src/controllers/FIleController.js
+++ backend/src/controllers/FIleController.js
@@ -4,7 +4,7 @@ const Box = require('../models/Box');
 class FileController {
     async store(req, res) {
         const box = await Box.findById(req.params.id);
-
+        
         const file = await File.create({
             title: req.file.originalname,
             path: req.file.key
